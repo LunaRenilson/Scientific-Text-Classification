b20a3e53bdab17af4f9f3c25c13926b9fda99be2
Renilson <r247358@dac.unicamp.br>
2025-04-02 16:08:40 -0300

    coluna areasCiencia limpa

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Astronomia
@@ -13,11 +14,44 @@ import matplotlib.pyplot as plt
 # Matemática
 # Computação
 
-disciplinas = ['F', 'B', 'Q', 'C', 'A']
+disciplinas = ['f', 'b', 'q', 'c']
 dados = pd.read_csv('dados.csv')
+dados['areasCiencia'] = dados['areasCiencia'].fillna('')
+dados['areasCiencia'] = dados['areasCiencia'].str.lower() # campos para minusculo
+
+dados = dados[
+    (~dados['areasCiencia'].str.contains(' ', na=False)) &
+    (~dados['areasCiencia'].str.contains('/', na=False)) &
+    (~dados['areasCiencia'].str.contains(',', na=False)) &
+    (~dados['areasCiencia'].str.contains('-', na=False)) &
+    (~dados['areasCiencia'].str.contains('_', na=False)) &
+    (~dados['areasCiencia'].str.contains(';', na=False))
+    ]
+
+dados.loc[dados['areasCiencia'].str.startswith('fís'), 'areasCiencia'] = 'f' # Renomeando campos da fisica para f
+
+dados.loc[
+    (dados['areasCiencia'].str.startswith('q', na=False)) &
+    (dados['areasCiencia'].str.len() < 8), 
+    'areasCiencia'
+    ] = 'q'
+
+dados.loc[
+    (dados['areasCiencia'].str.startswith('b', na=False)) &
+    (dados['areasCiencia'].str.len() < 9), 
+    'areasCiencia'
+    ] = 'b'
+
+dados.loc[
+    (dados['areasCiencia'].str.startswith('ger', na=False)) &
+    (dados['areasCiencia'].str.len() < 6), 
+    'areasCiencia'
+    ] = 'c'
+
+
 dados = dados.loc[dados['areasCiencia'].isin(disciplinas)]
 
-dados['areasCiencia'].hist(bins=5)
+sns.histplot(dados['areasCiencia'], bins=5)
 plt.show()
 
-# print(areaCiencia)
+dados.to_csv('dados_limpos.csv')
