3e4a00de1312ba87657a7bc48c09641dfcc050e3
LunaRenilson <renilson.c.luna@gmail.com>
2025-04-15 08:21:23 -0300

    Grafico de filtragem de dados gerado

--- dataFiltering.py
+++ dataFiltering.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 disciplinas = ['f', 'b', 'q', 'c']
 dados = pd.read_csv('dados.csv', usecols=['titulo', 'resumo', 'palavrasChave', 'areasCiencia'])
@@ -13,8 +14,9 @@ dados = dados[
     (~dados['areasCiencia'].str.contains('-', na=False)) &
     (~dados['areasCiencia'].str.contains('_', na=False)) &
     (~dados['areasCiencia'].str.contains(';', na=False)) &
-    (~dados['resumo'].str.contains('\?', na=False)) &
-    (~dados['palavrasChave'].str.contains('\?', na=False))
+    (~dados['areasCiencia'].str.contains(r'\+', na=False)) &
+    (~dados['resumo'].str.contains(r'\?', na=False)) &
+    (~dados['palavrasChave'].str.contains(r'\?', na=False))
 ]
 
 
@@ -48,4 +50,32 @@ dados.loc[
 # Selecionando disciplinas filtradas acima
 dados = dados.loc[dados['areasCiencia'].isin(disciplinas)]
 
-dados.to_csv('dados_v2.csv')
+# dados.to_csv('dados_v2.csv')
+
+
+# forgraph = dados = dados[
+#     (dados['areasCiencia'].str.startswith('fís', na=False)) |
+#     (dados['areasCiencia'].str.startswith('fis', na=False)) |
+#     (dados['areasCiencia'].str.startswith('quí', na=False)) |
+#     (dados['areasCiencia'].str.startswith('qui', na=False)) |
+#     (dados['areasCiencia'].str.startswith('bio', na=False)) |
+#     (dados['areasCiencia'].str.startswith('ger', na=False)) |
+#     (dados['areasCiencia'] == 'f') |
+#     (dados['areasCiencia'] == 'b') |
+#     (dados['areasCiencia'] == 'q') | 
+#     (dados['areasCiencia'] == 'c')
+# ]
+
+
+# freq = forgraph['areasCiencia'].value_counts()
+
+# # Plotando como gráfico de barras
+# freq.plot(kind='bar', edgecolor='black')
+
+# plt.xlabel('Categoria')
+# plt.ylabel('Frequência')
+# plt.title('Frequência de cada categoria')
+# plt.xticks(rotation=45)
+# plt.savefig('assets/hist_after-filtering.png')
+# plt.tight_layout()
+# plt.show()
