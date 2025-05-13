fdeba28c1e3267491eb270b99d27a1d2d97673fc
LunaRenilson <renilson.c.luna@gmail.com>
2025-05-13 16:59:31 -0300

    modelo simplificado com melhor resultado

--- neural-network/MLP.py
+++ neural-network/MLP.py
@@ -2,6 +2,7 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 
 # Libs de pre-processamento
@@ -37,30 +38,36 @@ y_int = le.fit_transform(df['classes_originais'])  # f → 0, q → 1, etc.
 # Converte para one-hot
 y = to_categorical(y_int)
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, stratify=y)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, stratify=y)
 
-model = Sequential([
-    Dense(512, activation='relu', input_shape=(X.shape[1],)),
-    Dropout(0.3),
-    Dense(128, activation='relu'),
-    Dropout(0.3),
-    Dense(4, activation='softmax')
-])
 
-model.compile(optimizer=Adam(learning_rate=0.001),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+def build_model():
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(X.shape[1],), kernel_regularizer=l2(0.001)),
+        Dropout(0.3),
+        Dense(32, activation='relu', kernel_regularizer=l2(0.001)),
+        Dropout(0.3),
+        Dense(16, activation='relu', kernel_regularizer=l2(0.001)),
+        Dropout(0.3),
+        Dense(4, activation='softmax')
+    ])
+
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+    
+    return model
 
 early_stop = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
+model = build_model()
 history = model.fit(
     X_train, y_train,
     validation_data=(X_val, y_val),
-    epochs=50,
+    epochs=20,
     batch_size=32,
     callbacks=[early_stop]
 )
-
 
 plt.plot(history.history['accuracy'], label='Treino')
 plt.plot(history.history['val_accuracy'], label='Validação')
